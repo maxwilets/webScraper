63817a19d72c993763bfc8459f024d945ac8fdd7
Max Wilets <mwilets@gmail.com>
2019-01-08 16:30:25 -0700

    server

---
+++ modules/Article.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+//Save a reference for schema construction
+const Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+const ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    note : {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+module.exports= Article;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const axios = require("axios");
 const cheerio = require("cheerio");
-const db = require("/models");
+const db = require("./modules");
 const PORT = 3000;
 const app = express();
 
@@ -25,44 +25,51 @@ mongoose.connect("mongodb://localhost/webScraper", {
 //Routes
 
 //A GET route for scraping the website
-app.get("/scrape", (req, res) => {
-    //first grab the body of the HTML with axios
-    axios.get("http://www.theberrics.com/").then(response => {
-        //Then loaded into cheerio and saved into $ for a shorthand selector
-        var $ = cheerio.load(response.data);
-        //grap every h3 within the artice
-        $("article h2").each(function (i, element) {
-            //save an empty result object
-            var result = {};
-            //add text and href
-            result.title = $(this)
-                .children("a")
-                .text();
-            result.link = $(this)
-                .children("a")
-                .attr("href");
-
-            //create a new Article using the 'result' object
-            db.Article.create(result)
-                .then(function (dbArticle) {
-                    console.log(dbArticle);
-                })
-                .catch(err => {err})
-        });
-        //Send a message to the client
-
-    res.send("Scrape Complete")
+app.get("/scrape", function(req, res) {
+    // First, we grab the body of the html with axios
+    axios.get("http://www.thrashermagazine.com/").then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+  
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("h4").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
+  
+        // Add the text and href of every link, and save them as properties of the result object
+        result.title = $(this)
+          .children("a")
+          .text();
+        result.link = "http:/www.thrashermagazine.com" + $(this)
+          .children("a")
+          .attr("href");
+  
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            // View the added result in the console
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            // If an error occurred, log it
+            console.log(err);
+          });
+      });
+  
+      // Send a message to the client
+      res.send("Scrape Complete");
     });
-});
+  });
 
 app.get("/articles", function(req,res){
     //Grab every document in the article
-    db.Articls.find({})
+    db.Article.find({})
       .then(function(dbArticle) {
           //if Articles are found send them back to the client
           res.json(dbArticle);
       })
-      .catch(err=>{err});
+      .catch(function(err){
+          res.json(err)});
 });
 
 //Route for grabbing Article by id, populate by note
@@ -73,7 +80,8 @@ app.get("/articles/:id", function(req,res){
       .then(function(dbArticle){
           res.json(dbArticle);
       })
-      .catch(err=>{err});
+      .catch(function(err){
+          res.json(err)});
 });
 
 app.post("/articles/:id", function(req,res){
@@ -83,7 +91,8 @@ app.post("/articles/:id", function(req,res){
         .then(function(dbArtice){
             res.json(dbArticle);
         })
-        .catch(err=>{err});
+        .catch(function(err){
+            res.json(err)});
 });
 
 app.listen(PORT, function(){
