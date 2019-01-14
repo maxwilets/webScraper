82b7ffc79b2a3e4d5801baf1690794c6d9060cf5
Max Wilets <mwilets@gmail.com>
2019-01-13 23:41:56 -0700

    heroku database

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ app.engine(
 }))
 app.set("view engine", "handlebars")
 //Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/webScraper", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true
 });
 
